40e839e4618fc0237c1dd2c27fc656e1bd0fe3c6
Ross <ross.joo@gmail.com>
2019-07-22 11:08:43 -0400

    electronBuilder config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,24 @@
 // const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-    // chainWebpack: config => {
-    //     config.externals(nodeExternals())
-    // },
+    chainWebpack: config => {
+        // config.externals(nodeExternals())
+    },
 
     pluginOptions: {
         electronBuilder: {
-            mainProcessWatch: ['src/server/server.js', 'src/server/teslacam.js']
+            builderOptions: {
+                appId: "com.tcmanager.app",
+                win: {
+                    target: ["portable"]
+                }
+            },
+
+            files: [
+
+            ],
+
+            mainProcessWatch: ['src/server/index.js', 'src/server/teslacam.js']
         }
     }
 }
